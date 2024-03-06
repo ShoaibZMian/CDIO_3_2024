636e3028c29a7142f411740fdf357cad7fee0a20
Khadejha Al-Body <khadejha_2400@hotmail.com>
2024-03-06 11:37:26 +0100

    Update runRobot.py
    Update i loop og detect ball func

--- runRobot.py
+++ runRobot.py
@@ -7,15 +7,29 @@ def receive_image():
     # her modtager vi billeden med cv2
     print("Image received")
    
+def fallback():
+    print("Error starting over again")
+    robot_loop()
 
-def detect_ball(image):
-    # Denne funktion ville anvende billedgenkendelse til at afgøre, om der er en bold
-    print("Detecting ball in the image")
+def detect_ball():
+    image = receive_image()# vi modtager et billede 
+    # image varibale oprettes ved hjælp af recive_image func
+
+    # vi skal bruge image her til at afgøre om ballfound er true eller false
+
+    ball_found = None
+    # her ville vi anvende billedgenkendelse til at afgøre, om der er en bold
+    if (ball_found == True) :
+        print("Detecting ball in the image")  
+    else :
+        print("No ball found") 
+        fallback()
     # returnere true når der er detected en bold på billedet af banen
-    return True
+    return ball_found
 
 def move_to_ball():
     print("Moving to the ball")
+
 
 def pick_up_ball():
     print("Picking up the ball")
@@ -33,23 +47,15 @@ def deliver_ball_to_goal():
 
 def robot_loop():
     start_robot()
-    while True:
-        image = receive_image()
-        if detect_ball(image):
-            move_to_ball()
-            pick_up_ball()
-            closest_goal = determine_closest_goal()
-            move_to_goal(closest_goal)
-            deliver_ball_to_goal()
-            receive_image()
-            if detect_ball(image):
-                continue
-            else:
-                print("No ball detected, shutting down.")
-                return False
-        else:
-            print("No ball detected, shutting down.")
-            return False
+    while detect_ball():
+        move_to_ball()
+        pick_up_ball()
+        closest_goal = determine_closest_goal()
+        move_to_goal(closest_goal)
+        deliver_ball_to_goal()
+        detect_ball()
+    else:
+        return fallback()
 
 # Start robottens loop
 robot_loop()
