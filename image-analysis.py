5c7335ba8cbbaf04e367056e9044a05d94123f2f
unknown <filip.merlung@gmail.com>
2024-04-11 15:20:26 +0200

    modified color isololation to show 4 pics

--- image-analysis.py
+++ image-analysis.py
@@ -4,33 +4,27 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
 
-img_path = os.path.join(os.path.dirname(__file__), "pic1.jpg")
-print(img_path)
-im = cv2.imread(img_path)
-im = cv2.resize(im, None, fx=0.7, fy=0.7)
-hsv_im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+img_name = "1.jpg"
+curdir = os.chdir("..\\data")
+img = cv2.imread(img_name)
+img = cv2.resize(img, None, fx=0.6, fy=0.6)
+hsv_im = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# analyzing colors
-# h, s, v = cv2.split(hsv_im)
-# fig = plt.figure()
-# axis = fig.add_subplot(1, 1, 1, projection="3d")
-# pixel_colors = im.reshape((np.shape(im)[0]*np.shape(im)[1], 3))
-# norm = colors.Normalize(vmin=-1.,vmax=1.)
-# norm.autoscale(pixel_colors)
-# pixel_colors = norm(pixel_colors).tolist()
-# 
-# axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
-# axis.set_xlabel("Hue")
-# axis.set_ylabel("Saturation")
-# axis.set_zlabel("Value")
-# plt.show()
+# These are wip values, might need tuning or rethink approach
+wall_hsv_lower = (0, 143, 169)
+wall_hsv_upper = (179, 255, 255)
+ball_hsv_lower = (0, 0, 191)
+ball_hsv_upper = (179, 120, 255)
 
-light_orange = (1, 70, 70)
-dark_orange = (255, 255, 255)
-mask = cv2.inRange(hsv_im, light_orange, dark_orange)
-result = cv2.bitwise_and(im, im, mask=mask)
+wall_mask = cv2.inRange(hsv_im, wall_hsv_lower, wall_hsv_upper)
+ball_mask = cv2.inRange(hsv_im, ball_hsv_lower, ball_hsv_upper)
+
+output = cv2.bitwise_and(img, img, mask=wall_mask)
+output = cv2.bitwise_or(img, img, mask=ball_mask)
 
 rgb_im = cv2.cvtColor(hsv_im, cv2.COLOR_HSV2BGR)
-cv2.imshow("image", result)
+cv2.imshow("wall mask", wall_mask)
+cv2.imshow("ball mask", ball_mask)
+cv2.imshow("masked output image", output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
