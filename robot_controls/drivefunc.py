827a68ccec576d27cffe4cb9b218d4d0e01e2c2c
Matthias Kobbernagel <Skumfidusendesigns@gmail.com>
2024-06-10 10:55:02 +0200

    Just changed all the robot controls to newest

--- robot_controls/drivefunc.py
+++ robot_controls/drivefunc.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
@@ -8,7 +7,6 @@ from pybricks.ev3devices import GyroSensor
 from pybricks.parameters import Direction
 import math
 
-
 ev3 = EV3Brick()
 
 # Tilslut gyrosensoren til port 1
@@ -17,44 +15,50 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 # Constants
-WHEEL_DIAMETER = 33.5  # Hjulets størrelse i mm
-AXEL_TRACK = 140  # Distancen mellem hjulene
+WHEEL_DIAMETER = 50.3  # Hjulets størrelse i mm
+AXEL_TRACK = 170  # Distancen mellem hjulene
 
 robot = DriveBase(left_motor, right_motor, WHEEL_DIAMETER, AXEL_TRACK)
 
-
 def drive_forward(distance_mm):
     gyro.reset_angle(0)
+    robot.reset()
     distance_cm = distance_mm * 100
 
-    while robot.distance() <= distance_cm:
-        correction = 0 - gyro.angle()
-        robot.drive(400, correction)  # 400 mm/seconds
+    while robot.distance() < distance_cm:
+        correction = gyro.angle()  # Negative feedback for correction
+        # correction = 0.5 * angle_error  # Apply proportional control to the correction
+        # correction = max(min(correction, 30), -30)
+
+        robot.drive(400, correction)
 
     robot.stop()
     left_motor.brake()
     right_motor.brake()
 
+
+    final_angle = gyro.angle()
+    text = "Final angle deviation: {} degrees".format(final_angle)
+    return text
 
 def drive_backward(distance_mm):
     while robot.distance() > -distance_mm:
-        robot.drive(-400, 0)  # 400 mm/seconds
+        robot.drive(-400, 0)
 
     robot.stop()
     left_motor.brake()
     right_motor.brake()
-
+    return "Driven backward mm"
 
 def turn_right(degrees):
     gyro.reset_angle(0)
-
-    while gyro.angle < degrees:
-        robot.drive(400, 10)
+    initial_angle = gyro.angle()
+    target_angle = initial_angle - degrees
+    while gyro.angle() > target_angle:
+        robot.drive(0, 150)
     robot.stop()
-    left_motor.brake()
-    right_motor.brake()
-    print(f"Robot turned {gyro.angle()}")
-
+    text = "Turned right {} degrees".format(gyro.angle())
+    return text
 
 def turn_left(degrees):
-    print("test")
+    return "Turned left degrees"
