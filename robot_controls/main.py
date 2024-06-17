86409d75702d76bb156e5e18c60d1ca42281b72b
Khadejha Al-Body <khadejha_2400@hotmail.com>
2024-06-17 11:12:52 +0200

    ip

--- robot_controls/main.py
+++ robot_controls/main.py
@@ -4,7 +4,7 @@ import threading
 import subprocess
 from commandparser import parse_and_execute
 
-bind_ip = "192.168.8.139"
+bind_ip = "172.20.10.4"
 bind_port = 8080  
 
 # create and bind a new socket
